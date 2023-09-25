bd672f85277db8cbacba75e5af175de3a40bf9d0
BrownTroutCoding <browntroutcoding@gmail.com>
2023-09-25 11:30:54 -0600

    change http to https

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var currencies = ['BTC', 'ADA', 'LINK', 'DOGE', 'ETH'];
     var api_key = '51d05fa3f63129165a8eadd9c49521e8';
-    var url = 'http://api.coinlayer.com/live';
+    var url = 'https://api.coinlayer.com/live';
 
 
     $.get(url, {access_key: api_key, symbols: currencies.join(',')}, function(data) {
