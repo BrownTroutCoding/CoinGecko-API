9dd457b5e396b2cce58945d1060c9a74771d0144
BrownTroutCoding <browntroutcoding@gmail.com>
2023-09-25 11:35:01 -0600

    reverted back to http

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     var currencies = ['BTC', 'ADA', 'LINK', 'DOGE', 'ETH'];
     var api_key = '51d05fa3f63129165a8eadd9c49521e8';
-    var url = 'https://api.coinlayer.com/live';
+    var url = 'http://api.coinlayer.com/live';
 
 
     $.get(url, {access_key: api_key, symbols: currencies.join(',')}, function(data) {
+        console.log("API Response:", data);
         // update each coin price in HTML
         $('.coin-price').each(function(index) {
             var coin = currencies[index];
